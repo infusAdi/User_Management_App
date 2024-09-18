21019b95719a6f598e578d1170e553962db22214
infusAdi <aditya.sen@infusai.com>
2024-09-18 14:58:48 +0530

    new trial

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <header className="header">
-        <h1 className="text-center text-3xl font-bold">User Manager App</h1>
+        <h1 className="text-center text-3xl font-bold">Manage App</h1>
         <nav className="h-12 m-auto flex justify-between m-8">
           <Button variant="contained" className="h-8">
             <Link to="/">Home</Link>
